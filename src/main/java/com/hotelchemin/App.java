d229f554bd1af46de7c300ff3d243c42613e53de
carlos.guayapero <carlos.guayapero@CARLOSGUZMAN.neoris.cxnetworks.net>
2020-07-02 07:51:48 -0300

    modificación de clase principal para despliegue en tomcat

--- src/main/java/com/hotelchemin/App.java
+++ src/main/java/com/hotelchemin/App.java
@@ -2,13 +2,23 @@ package com.hotelchemin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App 
+public class App extends SpringBootServletInitializer
 {
-    public static void main( String[] args )
+	//Archivos war desde tomcat
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(App.class);
+	}
+	
+	//Archivos JAR
+	public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
         System.out.println( "Hola tu aplicación de Maven esta andando correctamente" );
     }
+	
 }
